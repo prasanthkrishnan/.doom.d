9871fb07bd75b7907a4c72b60150f1ae117c60ba
zerocool <zerocool@zc-ws.lan>
2020-05-27 00:26:42 -0700

    Add projects directory in config
    Projects directory = ~/workspace
    Its the directory where projectile will search for projects

--- config.el
+++ config.el
@@ -1,6 +1,8 @@
 (setq user-full-name "Prasanth Krishnan"
       user-mail-address "knp281192@gmail.com")
 
+(setq projectile-project-search-path '("~/workspace/"))
+
 (setq doom-theme 'doom-one)
 
 (setq display-line-numbers-type 'relative)
