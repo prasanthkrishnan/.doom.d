3bff6d79ab20e7580affe944b74a916598773b54
zerocool <zerocool@zc-ws.lan>
2020-05-27 00:03:47 -0700

    Init config
    Enabled literate config

---
+++ config.el
@@ -0,0 +1,8 @@
+(setq user-full-name "Prasanth Krishnan"
+      user-mail-address "knp281192@gmail.com")
+
+(setq doom-theme 'doom-one)
+
+(setq display-line-numbers-type 'relative)
+
+(setq org-directory "~/org/")
